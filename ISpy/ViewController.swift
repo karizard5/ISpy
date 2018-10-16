7bb7ba7224d33fce3845fd2ab255333a66035ea6
Reid, Jon Carl <jreid2@hk220-mac1.local>
2018-10-15 17:04:39 -0700

    This Works...somewhat

--- ISpy/ViewController.swift
+++ ISpy/ViewController.swift
@@ -8,13 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    @IBOutlet var scrollViewOutlet: UIScrollView!
+    
+    @IBOutlet var imageViewOutlet: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scrollViewOutlet.delegate = self
+        updateZoomFor(size: view.bounds.size)
     }
 
 
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageViewOutlet
+    }
+
+
+    func updateZoomFor(size: CGSize){
+        let widthScale = size.width / imageViewOutlet.bounds.width
+        let heightScale = size.height / imageViewOutlet.bounds.height
+        let scale = min(widthScale, heightScale)
+        scrollViewOutlet.minimumZoomScale = scale
+    }
 }
+
 
